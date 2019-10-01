a1ee7eb9f41eaf83340774d61e150324084cf4f2
Tomislav Ljubej <tljubej@realnetworks.com>
2019-10-01 14:37:25 +0200

    Reroute SUB operation to modified ADD implementation

--- tensorflow/lite/delegates/gpu/gl/kernels/reduce_max.cc
+++ tensorflow/lite/delegates/gpu/gl/kernels/reduce_max.cc
@@ -60,7 +60,7 @@ class ReduceMax : public NodeShader {
 
     auto code =
     "for (int i = 0; i < $axis_count$; i++) {"
-    "  if ($input_data_0" + indexers_input[idx_from_axis] + "$ > $input_data_0" + indexers_output[idx_from_axis] + "$) {"
+    "  if ($input_data_0" + indexers_input[idx_from_axis] + "$[0] > $input_data_0" + indexers_output[idx_from_axis] + "$[0]) {"
     "    $input_data_0" + indexers_output[idx_from_axis] + "$ = $input_data_0" + indexers_input[idx_from_axis] + "$;"
     "  }"
     "}"
