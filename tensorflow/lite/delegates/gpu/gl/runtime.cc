cbff8d573acc95597336e45784074b39487cbf8b
Tomislav Ljubej <tljubej@realnetworks.com>
2019-10-17 11:38:12 +0200

    More debug outputs

--- tensorflow/lite/delegates/gpu/gl/runtime.cc
+++ tensorflow/lite/delegates/gpu/gl/runtime.cc
@@ -198,6 +198,9 @@ Status Runtime::AddProgram(const GlShader& shader,
     RETURN_IF_ERROR(program.SetParameter(parameter));
   }
 
+  std::cout << "workgroups size: " << num_workgroups.x << " " << num_workgroups.y << " " << num_workgroups.z << std::endl;
+
+
   programs_.emplace_back(
       CompiledProgramDescriptor{std::move(program), num_workgroups, {}});
 
