48b3709bef7e299e2f7aa60896887dae2a665633
Tomislav Ljubej <tljubej@realnetworks.com>
2019-10-23 18:38:24 +0200

    Add GL compute shader test

--- tensorflow/gl_cs_bench.cc
+++ tensorflow/gl_cs_bench.cc
@@ -119,7 +119,7 @@ void tryComputeShader(size_t compute_size, size_t workgroup_size)
     "void main()\n"
     "{\n"
     "    uint idx = gl_GlobalInvocationID.x;\n"
-    "    for (float i = 0; i < 50.0f; i+=0.25f) {\n"
+    "    for (float i = 0.0f; i < 50.0f; i+=0.25f) {\n"
     "        output0.data[idx] = i;\n"
     "    }\n"
     "}\n";
